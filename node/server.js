4b807ba354581e0a2427286ce81cb9ba29109a87
MinWoo Song <smw0807@gmail.com>
2019-03-02 23:04:31 +0900

    미들웨어 테스트

--- node/server.js
+++ node/server.js
@@ -4,6 +4,15 @@ var express = require("express");
 
 //웹서버 생성
 var app = express();
+app.use(function (request, response, next) {
+    console.info("use check..");
+    console.info(request);
+    console.info(response);
+    console.info(next);
+    next();
+});
+app.use(function (request, response, next) {});
+app.use(function (request, response, next) {});
 
 //웹서버 실행
 http.createServer(app).listen(8085, function () {
