12b2bccef2f0d92bf83a937d8657979bcaae87e8
MinWoo Song <smw0807@gmail.com>
2019-03-03 19:15:05 +0900

    기초틀 구상

--- node/server.js
+++ node/server.js
@@ -4,17 +4,12 @@ var express = require("express");
 
 //웹서버 생성
 var app = express();
-app.use(function (request, response, next) {
-    console.info("use check..");
-    console.info(request);
-    console.info(response);
-    console.info(next);
-    next();
-});
+app.use(function (request, response, next) {});
 app.use(function (request, response, next) {});
 app.use(function (request, response, next) {});
 
 //웹서버 실행
 http.createServer(app).listen(8085, function () {
     console.log('Server Running !!!');
+    //setInterval (() => {}, 5000);
 });
