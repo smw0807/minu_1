2b8a0d3426901ae1c9b6a747d98e7220b5db5a4f
MinWoo Song <smw0807@gmail.com>
2019-03-06 18:12:54 +0900

    테스트

--- node/server.js
+++ node/server.js
@@ -1,6 +1,7 @@
 //모듈 추출
 var http = require("http");
 var express = require("express");
+var main = require("./web/js/main");
 
 //웹서버 생성
 var app = express();
@@ -11,5 +12,8 @@ app.use(function (request, response, next) {});
 //웹서버 실행
 http.createServer(app).listen(8085, function () {
     console.log('Server Running !!!');
+    var tt = main.setDate();
+    
+    console.log(tt);
     //setInterval (() => {}, 5000);
 });
--- node/web/js/main.js
+++ node/web/js/main.js
@@ -1,1 +1,7 @@
 //메인 스크립트
+exports.setDate = function () {
+    console.info("in test");
+    var date = new Date();
+    var result = date.getTime();
+    return result;
+};
