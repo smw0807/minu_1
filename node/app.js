6ffa83b9ad643f7283a087217297b885c14bfcbd
MinWoo Song <smw0807@gmail.com>
2019-04-04 14:52:23 +0900

    app.js test

---
+++ node/app.js
@@ -0,0 +1,12 @@
+var http = require("http");
+var express = require("express");
+
+var app = express();
+app.use(express.static('web'));
+
+app.use('/main', require('./web/html/main.html'));
+
+
+http.createServer(app).listen(8085, function () {
+    console.log("Node Server Start!!!!");
+});
