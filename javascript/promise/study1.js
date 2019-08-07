9021c73213c669f61cb2923bc791908059ebbda3
smw0807 <smw0807@gmail.com>
2019-08-07 13:58:54 +0900

    참고 사이트 추가

--- javascript/promise/study1.js
+++ javascript/promise/study1.js
@@ -1,4 +1,5 @@
 /**
  * 참고 URL1 : https://joshua1988.github.io/web-development/javascript/promise-for-beginners/
  * 참고 URL2 : https://programmingsummaries.tistory.com/325
+ * 참고 URL3 : https://han41858.tistory.com/11
  */
