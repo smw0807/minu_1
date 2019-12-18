466dbdabc029dc482ee60274207a500517f18072
smw0807 <smw0807@gmail.com>
2019-12-18 16:02:32 +0900

    변수 선언 테스트

---
+++ javascript/scope_closer/scope.js
@@ -0,0 +1,29 @@
+//var, let, const test
+
+// var a = "aa";
+// var a = "a";
+// a = "aaaa";
+// console.log(a); //aaaa
+//같은 변수로 선언하면 마지막껄로 됨
+
+// let a = 'aa';
+// let a = 'a'; //error
+// a = 'aaaa';
+// console.log(a);
+//같은 변수로 선언 못함
+
+// let a = 'aa';
+// a = 'aaaa';
+// console.log(a); //aaaa
+//재정의는 할 수 있음
+
+// const a = 'aa';
+// const a = 'a'; //error
+// a = 'aaaa';
+// console.log(a);
+//같은 변수로 선언 못함
+
+// const a = 'aa';
+// a = 'aaaa'; //error
+// console.log(a);
+//재정의 못함
