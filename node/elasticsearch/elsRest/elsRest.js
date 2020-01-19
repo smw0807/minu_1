556757066716eea2f8321cf84a92d786c723af5e
smw0807 <smw0807@gmail.com>
2020-01-19 21:47:54 +0900

    row, insert 추가

--- node/elasticsearch/elsRest/elsRest.js
+++ node/elasticsearch/elsRest/elsRest.js
@@ -11,6 +11,7 @@ exports.getCount = async function (idx) {
     } catch (err) {
         console.log('error!!');
         console.log(err);
+        rs = err;
     }
     return rs;
 };
@@ -28,14 +29,81 @@ exports.search = async function (idx, query) {
         rs = rs.hits.hits;
     } catch (err) {
         console.log('error!!');
-        console.log(err);
+        // console.log(err);
+        rs = err;
     }
     return rs;
 }
 //todo row
+exports.row = async function (idx, id) {
+    const index = idx;
+    const _id = id;
+    let rs;
+    var param = {
+        index: index,
+        type: '_doc',
+        id: _id
+    };
+    try {
+        rs = await client.get(param);
+    } catch (err) {
+        console.log('error!!');
+        // console.log(err);
+        rs = err;
+    }
+    return rs;
+};
 
 //todo indsert
+exports.insert = async function (idx, data) {
+    let rs;
+    //data
+    let user_id = data.user_id;
+    let user_nm = data.user_nm;
+    let user_pw = data.user_pw;
+    let user_email = data.user_email;
+    let user_tel = data.user_tel;
+    let user_hp = data.user_hp;
+    let user_mk_dt = data.user_mk_dt;
+    //id 중복체크
+    let chk = {
+        index: idx,
+        type: '_doc',
+        body: {query: {term: {USER_ID: user_id}}}
+    };
+    var cnt = await client.count(chk);
+    if (cnt.count != 0) {
+        rs = false;
+    } else {
+        var param = {
+            index: idx,
+            type: '_doc',
+            id: user_id,
+            body: {
+                USER_ID: user_id,
+                USER_NM: user_nm,
+                USER_PW: user_pw,
+                USER_EMAIL: user_email,
+                USER_TEL: user_tel,
+                USER_HP: user_hp,
+                USER_MK_DT: user_mk_dt,
+                IS_USE: true
+            }
+        };
+        try {
+            rs = await client.create(param);
+        } catch (err) {
+            console.log("error!!");
+            rs = err;
+        }
+    }
+    return rs;
+};
 
 //todo update
+exports.update = async function (idx, id, data) {
+    let rs;
+    
+}
 
 //todo delete
--- node/elasticsearch/elsTest.js
+++ node/elasticsearch/elsTest.js
@@ -16,4 +16,31 @@ async function search (idx, query) {
     })
     console.log(obj);
 };
-search('user', query.search);
+//search('user', query.search);
+
+async function getRow (idx, id) {
+    var row = await els.row(idx, id);
+    console.log(row);
+};
+// getRow('user', 'smw0807');
+
+async function insert (idx, data) {
+    var rs = await els.insert(idx, data);
+    if (!rs) {
+        console.log("이미 존재하는 아이디 입니다.");
+    } else {
+        console.log(rs);
+    }
+    // console.log(rs);
+}
+var data = {
+    user_id: "minu0807",
+    user_nm: "송민우",
+    user_pw: "123123",
+    user_email: "smw0807@naver.com",
+    user_tel: "02-1234-1234",
+    user_hp: "010-1234-1234",
+    user_mk_dt: "2020-01-19 19:03:11+0900",
+    is_use: true
+}
+// insert('user', data);
