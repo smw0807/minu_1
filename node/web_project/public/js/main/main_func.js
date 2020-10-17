ccb93600858605e26078cbb7bad720bc0e77fb4f
smw0807 <smw0807@gmail.com>
2020-10-17 23:10:25 +0900

    불필요 소스 제거

--- node/web_project/public/js/main/main_func.js
+++ node/web_project/public/js/main/main_func.js
@@ -1,11 +1,3 @@
-// $('#home').addClass('active');
-// $('#javascript').removeClass('active');
-
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-})
-
-$('#btn_test').on('click', function () {
-    console.log("??");
-    console.log("아오 복잡해");
 })
