356e1a15882c88c502e26292284cd1167f191004
smw0807 <smw0807@gmail.com>
2020-10-27 21:03:13 +0900

    디렉티브 연습

--- node/web_project/public/js/vue/test6.js
+++ node/web_project/public/js/vue/test6.js
@@ -2,4 +2,28 @@
  * 디렉티브
  */
 
- 
+ var app1 = new Vue({
+     el:"#app1",
+     data:{
+         url:'https://www.naver.com/'
+     },
+     methods: {
+         doSomething: function () {
+             console.log("doSomething!!");
+         }
+     }
+ });
+
+var app2 = new Vue({
+    el:'#app2',
+    data:{
+        url:'/'
+    },
+    methods: {
+        doSomething: function () {
+            console.log("doSomething!! url change!!!!!");
+            this.url = '/js-datatables';
+            
+        }
+    }
+});
