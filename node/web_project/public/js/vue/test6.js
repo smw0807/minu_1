f465a2636dddfabc50c95c500595bd1204ac00c0
smw0807 <smw0807@gmail.com>
2020-10-28 21:52:28 +0900

    디렉티브 연습 v-if, v-bind

--- node/web_project/public/js/vue/test6.js
+++ node/web_project/public/js/vue/test6.js
@@ -23,7 +23,36 @@ var app2 = new Vue({
         doSomething: function () {
             console.log("doSomething!! url change!!!!!");
             this.url = '/js-datatables';
-            
+        }
+    }
+});
+
+var app3 = new Vue({
+    el:'#app3',
+    data: {
+        id: '',
+        pass: ''
+    },
+    methods: {
+        sendText: function () {
+            var id = $('#id').val();
+            var pass = $('#pass').val();
+            if (id == '') {
+                alert("ID를 입력해주세요.");
+                return;
+            }
+            if (pass == '') {
+                alert("PW를 입력해주세요.");
+                return;
+            }
+            this.id = id;
+            this.pass = pass;
+        },
+        reset: function () {
+            $('#id').val('');
+            $('#pass').val('');
+            this.id = '';
+            this.pass = '';
         }
     }
 });
