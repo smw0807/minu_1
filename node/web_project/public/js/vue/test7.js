7865d9340d3f29e5e444dba9e5e60eba2a29e38e
smw0807 <smw0807@gmail.com>
2020-10-29 20:33:12 +0900

    computed

--- node/web_project/public/js/vue/test7.js
+++ node/web_project/public/js/vue/test7.js
@@ -1,1 +1,13 @@
 //https://kr.vuejs.org/v2/guide/computed.html
+
+var app1 = new Vue({
+    el:"#app1",
+    data: {
+        message: "안녕하세요"
+    },
+    computed: {
+        reversedMessage: function () {
+            return this.message.split('').reverse().join('');
+        }
+    },
+});
