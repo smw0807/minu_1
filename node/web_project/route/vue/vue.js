0fe76784bf68c9923cc145d4256628f6f8157f80
smw0807 <smw0807@gmail.com>
2020-11-04 20:28:30 +0900

    오타 수정 및 클래스와 스타일 바인딩 연습 페이지 추가

--- node/web_project/route/vue/vue.js
+++ node/web_project/route/vue/vue.js
@@ -73,4 +73,15 @@ module.exports = function (app) {
 
         })
     });
+    app.get('/vue-test8', function (req, res) {
+        var sess = req.session;
+        res.render('vue/test7', {
+            title: "Vue 클래스와 스타일 바인딩",
+            length: 5,
+            name: sess.name,
+            username: sess.username,
+            pageType: pageType,
+
+        })
+    });
 };
