435dbc304405b4b87c020e7f60b4f48bb8712718
smw0807 <smw0807@gmail.com>
2020-11-05 21:06:40 +0900

    배열구문 및 응용 몇가지 해봄

--- node/web_project/public/js/vue/test8.js
+++ node/web_project/public/js/vue/test8.js
@@ -3,17 +3,24 @@ var app1 = new Vue({
     data: {
         isActive: false,
         isActive2: false,
-        isBtn : true
-        
+        isBtn : true,
+        panelStyle : 'panel-default',
+        test: 'bg-danger'
     },
     methods: {
         goActive: function () {
-            console.log("goActive");
             this.isActive = true;
         },
         backActive: function () {
-            console.log("backActive");
             this.isActive = false;
+        },
+        btn3 : function () {
+            this.isActive2 = (this.isActive2 == true ? false : true);
+            this.isBtn = (this.isBtn == true ? false : true);
+        },
+        btnColor: function (data) {
+            this.panelStyle = 'panel-' + data;
+            this.test = 'bg-' + data;
         }
     }
 });
