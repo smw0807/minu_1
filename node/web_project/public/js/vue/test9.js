8496e115fecd9fa04ecae0f81e1d580c5a0980b9
smw0807 <smw0807@gmail.com>
2020-11-15 18:06:08 +0900

    조건부 렌더링 끝

--- node/web_project/public/js/vue/test9.js
+++ node/web_project/public/js/vue/test9.js
@@ -15,3 +15,42 @@ var app1 = new Vue({
         }
     }
 });
+
+var app2 = new Vue({
+    el: '#app2',
+    data:{
+        loginType: 'username'
+    },
+    methods: {
+        changeLoginType: function() {
+            console.log('changeLoginType');
+            var type = this.loginType;
+            if (type == 'username') {
+                this.loginType = 'email';
+            } else {
+                this.loginType = 'username';
+            }
+        },
+        getData: function() {
+            console.log('getData');
+            var name = $('#user_name').val();
+            var email = $('#user_email').val();
+            console.log(name);
+            console.log(email);
+        }
+    }
+});
+
+var app3 = new Vue({
+    el:"#app3",
+    data: {
+        showTest : false
+    },
+    methods: {
+        showChange: function () {
+            console.log('showChange');
+            this.showTest = (this.showTest == true ? false : true);
+            console.log(this.showTest);
+        }
+    }
+});
