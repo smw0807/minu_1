794f50dd4b145ce665382ed3b1aa47847b7674ba
smw0807 <smw0807@gmail.com>
2020-11-21 19:36:16 +0900

    객체 변경감지, 필터링-정렬에 대해

--- node/web_project/public/js/vue/test10.js
+++ node/web_project/public/js/vue/test10.js
@@ -51,7 +51,49 @@ var app4 = new Vue({
 });
 // app4.items[1] = 'bb';
 // Vue.set(app4.items, 1, 'bb');
-app4.$set(app4.items, 1, 'bbbb');
+// app4.$set(app4.items, 1, 'bbbb');
 // app4.items.splice(1, 1, 'bbb');
 // app4.items.length = 3;
 // app4.items.splice(3);
+
+var app5 = new Vue({
+    el:'#app5',
+    data: {
+        userProfile: {
+            name: 'minwoo'
+        }
+    }
+});
+app5.$set(app5.userProfile, "now", "study");
+Vue.set(app5.userProfile, "age", "29");
+Object.assign(app5.userProfile, {
+    test1: 'test1',
+    test2: 'test2'
+});
+app5.userProfile.iiii = 'TTTT'; //이건 안된다는거 아니었나? 되는데....
+app5.userProfile = Object.assign({}, app5.userProfile, {
+    test11: 'test11',
+    test22: 'test22',
+    test33: 'test33'
+})
+
+var app6 = new Vue({
+    el: '#app6',
+    data: {
+        numbers: [1,4,2,6,5,3]
+    },
+    computed: {
+        evenNumbers: function () {
+            return this.numbers.filter(function (number) {
+                return number % 2 === 0;
+            })
+        }
+    },
+    methods: {
+        even: function(num) {
+            return num.filter(function (number) {
+                return number % 2 === 0;
+            })
+        }
+    }
+});
