2444baa1e60cb856dedcd9086fd51c6acbb5f823
smw0807 <smw0807@gmail.com>
2020-12-25 20:43:49 +0900

    컴포넌트 마무리 (책으로 변경함..공식 API 홈 설명이 너무 부실함)

--- node/web_project/public/js/vue/test13.js
+++ node/web_project/public/js/vue/test13.js
@@ -288,6 +288,14 @@ Vue.component('child-component2', {
         }
     }
 })
+Vue.component('slot-component', {
+    template: `
+    <div>
+        <h2>나는 자식 컴포넌트의 제목입니다.</h2>
+        <slot>제공된 컨텐츠가 없는 경우에만 보실 수 있습니다.</slot>
+    </div>
+    `
+})
 
 var app4 = new Vue({
     el: '#app4',
