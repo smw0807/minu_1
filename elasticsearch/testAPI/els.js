0e78ca49382563bda743e19a45b065d22faf8f63
smw0807 <smw0807@gmail.com>
2021-03-24 22:18:10 +0900

    내 개인 데스크톱에 도커 엘라스틱, 몽고디비 이미지 띄워서 셋팅 다시함

--- elasticsearch/testAPI/els.js
+++ elasticsearch/testAPI/els.js
@@ -1,6 +1,7 @@
 const elasticsearch = require('elasticsearch');
 
-const url = 'https://test:test@192.168.1.23:9200';
+// const url = 'https://test:test@192.168.1.23:9200';
+const url = 'http://localhost:9200';
 
 const es_client = new elasticsearch.Client({hosts: url});
 
