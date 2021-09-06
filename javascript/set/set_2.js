2469f8859b150d216f70c2a31fa7a3f582f8a3df
smw0807 <smw0807@gmail.com>
2021-09-06 17:16:32 +0900

    집에가서 해볼거 까먹기전에 커밋

---
+++ javascript/set/set_2.js
@@ -0,0 +1,9 @@
+/**
+ * 집에서 해볼것
+ * set() 함수를 이용해 배열내 중복 데이터 확인하는걸 해봤었는데.
+ * 오브젝트 형식의 배열도 체크가 가능한지 확인해보기
+ * 확인이 가능하다면
+ * 1. 오브젝트 내 모든 필드와 데이터가 일치해야하는가?
+ * 2. 아니면 특정 필드 데이터만 일치하면 되는지 할 수 있는가?
+ * https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Set
+ */
