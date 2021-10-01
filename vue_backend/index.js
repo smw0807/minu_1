8778ce492c0eca4aafbab6980995edfbb6242555
smw0807 <smw0807@gmail.com>
2021-10-01 21:16:28 +0900

    vue_backend > vue-cli-vuetify프로젝트에서 테스트할 api url

--- vue_backend/index.js
+++ vue_backend/index.js
@@ -7,8 +7,9 @@ const app = express();
 
 require('dotenv').config();
 
-const server = app.listen(process.env.prot, function() {
+const server = app.listen(process.env.port, function() {
   console.log ("Server Start... [" + util.dateFormat('yyyy-MM-dd HH:mm:ss E') + ']');
+  console.log('service port : ', process.env.port);
 });
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -22,6 +23,15 @@ app.post('/api/test', (req, res) => {
 //test nuxt에서 serverMiddleware 값이 있어도 탈 수 있는지 확인용
 app.post('/express/test', (req, res) => {
   console.log('/express/test....');
+  res.send('API Success!!');
+})
+//vue cli 테스트 용
+app.post('/api1/test', (req, res) => {
+  console.log('/api1/test....');
+  res.send('API Success!!');
+})
+app.post('/api2/test', (req, res) => {
+  console.log('/api2/test....');
   res.send('API Success!!');
 })
 
