0db94561e4ba6e36004ae3ff42c72f7f899cd34b
smw0807 <smw0807@gmail.com>
2021-11-08 21:13:08 +0900

    노드 엘라스틱 관련 파일 수정 및 추가

---
+++ node/elasticsearch/client.js
@@ -0,0 +1,8 @@
+var elasticsearch = require('elasticsearch');
+require('dotenv').config();
+
+var client = new elasticsearch.Client({
+    host: ['http://' + process.env.els_id + ':' + process.env.els_pw + '@' + process.env.els_ip + ':' + process.env.els_port]
+});
+
+module.exports = client;
