72122b893d2bc6028550097e82e92ba79dc90ccf
smw0807 <smw0807@gmail.com>
2021-11-12 09:01:59 +0900

    asyncForEach

--- node/es_data_make_bulk/run_bulk.js
+++ node/es_data_make_bulk/run_bulk.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 const es_client = require('./client');
 
+async function asyncForEach (array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
+  };
+
 async function run() {
   try {
     const list = fs.readdirSync('./_bulk/');
-    list.forEach( (name) => {
+    asyncForEach(list, async (name) => {
+      console.log(name);
       let data = fs.readFileSync('./_bulk/' + name, 'utf8');
       let bulk = await es_client.bulk({
         body: data
