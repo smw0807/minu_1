25b1d8ad819180b5125acd931efdf465582e5a25
smw0807 <smw0807@gmail.com>
2021-11-25 17:33:51 +0900

    인덱스 범위 검색 및 존재 인덱스 반환

--- node/elasticsearch/client.js
+++ node/elasticsearch/client.js
@@ -2,7 +2,7 @@ var elasticsearch = require('elasticsearch');
 require('dotenv').config();
 
 var client = new elasticsearch.Client({
-    host: ['http://' + process.env.els_id + ':' + process.env.els_pw + '@' + process.env.els_ip + ':' + process.env.els_port]
+    host: [process.env.protocol + '://' + process.env.els_id + ':' + process.env.els_pw + '@' + process.env.els_ip + ':' + process.env.els_port]
 });
 
 module.exports = client;
---
+++ node/elasticsearch/run_index_check.js
@@ -0,0 +1,95 @@
+const es_client = require('./client');
+const moment = require('moment');
+
+function type (type) {
+  let rt = '';
+  switch(type) {
+    case 'YYYYMMDD':
+      rt = 'days';
+      break
+    case 'YYYYMM':
+      rt = 'month';
+      break
+    case 'YYYY':
+      rt = 'years';
+      break
+  }
+  return rt;
+}
+/**
+ * 년월일, 년월, 년 식의 인덱스 여러개를 조회하기 전 인덱스 유무를 체크해서
+ * 존재하는 인덱스들로 반환시키기
+ * @param {*} idx_name : index name
+ * @param {*} sdt : start date
+ * @param {*} edt : end date
+ * @param {*} format : date format
+ * @returns 
+ */
+async function run(idx_name, sdt, edt, format) {
+  let rt = false;
+  try {
+    console.log(idx_name, sdt, edt, format);
+    if (sdt === undefined && edt === undefined) { //인덱스 1개 검색
+      const rs = await es_client.indices.exists({index: idx_name});
+      if (rs) {
+        return idx_name;
+      }
+    } else if (sdt === edt) { //시작날짜, 종료날짜 값이 같은 경우
+      const rs = await es_client.indices.exists({index: idx_name + sdt});
+      if (rs) {
+        return idx_name;
+      }
+    } else if (sdt !== edt && sdt !== undefined && edt !== undefined && format !== undefined) { //범위일 때
+      if (sdt.indexOf('-') !== -1) {
+        sdt = sdt.replace(/-/g, '');
+      }
+      if (edt.indexOf('-') !== -1) {
+        edt = edt.replace(/-/g, '');
+      }
+      let start = moment(sdt);
+      let end = moment(edt);
+      let now = start.clone();
+      let result = [];
+      while(now.isSameOrBefore(end)) {
+        const index_name =  idx_name + now.format(format);
+        const rs = await es_client.indices.exists({index: index_name});
+        if (rs) {
+          result.push(index_name);
+        }
+        now.add(1, type(format));
+      }
+      console.log(result);
+      console.log(result.toString()); //배열을 스트링으로 바꿔주면 이렇게 됨
+      /**
+       * [ 'ni_raw_flw-20211118', 'ni_raw_flw-20211119', 'ni_raw_flw-20211120' ]
+       * ni_raw_flw-20211118,ni_raw_flw-20211119,ni_raw_flw-20211120
+       */
+      return result.toString();
+    } else {
+      console.log('형식 확인 필요');
+      return false;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+  return rt;
+}
+
+
+async function test_function() {
+  try {
+    const chk = await run('ni_raw_threat-20211125');
+    // const chk = await run('ni_raw_flw-');
+    // const chk = await run('ni_raw_flw-20211120');
+    // const chk = await run('ni_raw_flw-', '2021-11-20', '2021-11-25', 'YYYYMMDD');
+    // const chk = await run('ni_stat_month-', '2020', '2021', 'YYYY');
+    // const chk = await run('ni_raw_flw-', '20211117', '20211120', 'YYYYMMDD');
+    console.log(chk);
+    if (!chk) {
+      //elasticsearch event...
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+test_function();
