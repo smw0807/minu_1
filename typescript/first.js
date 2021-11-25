999c5e3afb32a0ff8b261ece0e0ae40acb5f85cf
smw0807 <smw0807@gmail.com>
2021-11-25 15:45:22 +0900

    타입스크립트 테스트

---
+++ typescript/first.js
@@ -0,0 +1,9 @@
+// myFirstTypescript.ts
+var str = "happy CODEing";
+var Coffee = /** @class */ (function () {
+    function Coffee(name, temperature) {
+        this.name = name;
+        this.temperature = temperature;
+    }
+    return Coffee;
+}());
---
+++ typescript/first.ts
@@ -0,0 +1,11 @@
+// myFirstTypescript.ts
+const str: string = "happy CODEing";
+
+class Coffee {
+  private name: string;
+  private temperature: string;
+  constructor(name: string, temperature: string) {
+    this.name = name;
+    this.temperature = temperature;
+  }
+}
