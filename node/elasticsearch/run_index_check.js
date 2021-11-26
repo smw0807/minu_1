8d9e2e9b9d3f488a999c0d1631309d43b32ce8ce
smw0807 <smw0807@gmail.com>
2021-11-26 10:26:52 +0900

    인덱스 체크 로직 수정

--- node/elasticsearch/run_index_check.js
+++ node/elasticsearch/run_index_check.js
@@ -35,9 +35,15 @@ async function run(idx_name, sdt, edt, format) {
         return idx_name;
       }
     } else if (sdt === edt) { //시작날짜, 종료날짜 값이 같은 경우
+      if (sdt.indexOf('-') !== -1) {
+        sdt = sdt.replace(/-/g, '');
+      }
+      if (format !== undefined) { //같은데 날짜 포맷이 있을 경우
+        sdt = sdt.substr(0, format.length);
+      }
       const rs = await es_client.indices.exists({index: idx_name + sdt});
       if (rs) {
-        return idx_name;
+        return idx_name + sdt;
       }
     } else if (sdt !== edt && sdt !== undefined && edt !== undefined && format !== undefined) { //범위일 때
       if (sdt.indexOf('-') !== -1) {
@@ -58,15 +64,15 @@ async function run(idx_name, sdt, edt, format) {
         }
         now.add(1, type(format));
       }
-      console.log(result);
-      console.log(result.toString()); //배열을 스트링으로 바꿔주면 이렇게 됨
+      // console.log(result);
+      // console.log(result.toString()); //배열을 스트링으로 바꿔주면 이렇게 됨
       /**
        * [ 'ni_raw_flw-20211118', 'ni_raw_flw-20211119', 'ni_raw_flw-20211120' ]
        * ni_raw_flw-20211118,ni_raw_flw-20211119,ni_raw_flw-20211120
        */
       return result.toString();
     } else {
-      console.log('형식 확인 필요');
+      console.log('인덱스 체크 함수 인자값 확인 필요 ', idx_name, sdt, edt, format);
       return false;
     }
   } catch (err) {
@@ -78,15 +84,16 @@ async function run(idx_name, sdt, edt, format) {
 
 async function test_function() {
   try {
-    const chk = await run('ni_raw_threat-20211125');
+    // const chk = await run('ni_raw_threat-20211125');
     // const chk = await run('ni_raw_flw-');
     // const chk = await run('ni_raw_flw-20211120');
     // const chk = await run('ni_raw_flw-', '2021-11-20', '2021-11-25', 'YYYYMMDD');
     // const chk = await run('ni_stat_month-', '2020', '2021', 'YYYY');
-    // const chk = await run('ni_raw_flw-', '20211117', '20211120', 'YYYYMMDD');
+    const chk = await run('ni_stat_day-', '2021-11-20', '2021-11-20', 'YYYYMM');
     console.log(chk);
     if (!chk) {
       //elasticsearch event...
+      console.log('not exists index');
     }
   } catch (err) {
     console.error(err);
