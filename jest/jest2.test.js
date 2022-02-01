b8f941acd1dbd396f1deaa02c26636a5fd5e00bf
smw0807 <smw0807@gmail.com>
2022-02-01 17:40:15 +0900

    jest toEqual 테스트... 어떻게 obj를 테스트하는 거지??

--- jest/jest2.test.js
+++ jest/jest2.test.js
@@ -1,5 +1,22 @@
+const { func1 } = require('./test1');
 describe("테스트....", () => {
   test('1 + 1 = 2?', () => {
-    expect(1 + 1).toBe(3);
+    expect(1 + 1).toBe(2);
+  })
+
+  //toBeUndefined
+  test('예상한 값이 Undefined 인지 확인 ', () => {
+    const a = {};
+    expect(a.aa).toBeUndefined();
+  })
+
+  test('toBeTruthy ?', () => {
+    const a = true;
+    expect(a).toBeTruthy();
+  })
+
+  test('toEqual', () => {
+    const a = false;
+    expect(func1(a)).toEqual(obj.error);
   })
 })
---
+++ jest/test1.js
@@ -0,0 +1,19 @@
+function func1(v) {
+  console.log('func1 : ', v);
+  let result = {};
+  if (v) {
+    console.log('true');
+    result.error == false;
+    result.a = 'aaaa';
+    result.b = 'bbbb';
+    result.c = 1111;
+  } else {
+    console.log('false');
+    result.error = true;
+  }
+  return result;
+}
+
+module.exports = {
+  func1
+}
