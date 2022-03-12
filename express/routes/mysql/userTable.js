aca5e3db1db3a8f93f7acb2e5e4a8f22824a1d78
smw0807 <smw0807@gmail.com>
2022-03-12 18:06:19 +0900

    express > mysql update 로직

--- express/routes/mysql/userTable.js
+++ express/routes/mysql/userTable.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mysql = require('../../mysql');
 const { encryptPassword } = require('../../utils/authenticate');
 
+const table = 'tb_user';
 router.post('/insert', async (req, res) => {
   let rt = {
     ok : false,
@@ -10,13 +11,12 @@ router.post('/insert', async (req, res) => {
     result : null
   }
   const params = req.body;
-  console.table(params);
   let conn = null;
   try {
     if (params.user_id === undefined || params.user_id === '') throw '아이디 없음';
     if (params.user_nm === undefined || params.user_nm === '') throw '이름 없음';
     if (params.user_pw === undefined || params.user_pw === '') throw '패스워드 없음';
-    const sql = `INSERT INTO tb_user (user_id, user_nm, user_pw, user_addr, user_mk_dt, user_upd_dt, is_use) VALUES (
+    const sql = `INSERT INTO ${table} (user_id, user_nm, user_pw, user_addr, user_mk_dt, user_upd_dt, is_use) VALUES (
       "${params.user_id}",
       "${params.user_nm}",
       "${encryptPassword(params.user_pw)}",
@@ -41,6 +41,63 @@ router.post('/insert', async (req, res) => {
     rt.result = err.message;
     await conn.rollback(); //rollback
     conn.release();
+  }
+  res.send(rt);
+})
+/**
+UPDATE 테이블명 SET 컬럼1 = 수정값1 [, 컬럼2 = 수정값2 ...] [WHERE 조건];
+예제 : 테이블 데이터 전체 수정
+UPDATE table_name SET country = '대한민국';
+예제 : 테이블 데이터 일부 수정
+UPDATE table_name SET name = '테스트 변경', country = '대한민국' WHERE id = 1105;
+ */
+router.post('/update', async (req, res) => {
+  let rt = {
+    ok: false,
+    msg: '',
+    result: null
+  }
+  const params = req.body;
+  let conn = null;
+  try {
+    let sql = `UPDATE ${table} SET `;
+    //---------------
+    //배열을 toString() 처리하면 콤마 처리가 편리해서 이렇게 만들어 봤음
+    let tmp = [];
+    if (params.user_nm) {
+      tmp.push(`user_nm="${params.user_nm}"`);
+    }
+    if (params.user_pw) {
+      tmp.push(`user_pw="${params.user_pw}"`);
+    }
+    if (params.user_addr) {
+      tmp.push(`user_addr="${params.user_addr}"`);
+    }
+    //--------------
+    // console.log(tmp.toString());
+    if (tmp.length == 0) {
+      throw { message : '입력된 값이 없음'};
+    } else {
+      sql += `${tmp.toString()} WHERE user_id="${params.user_id}"`;
+      // console.log(sql);
+      conn = await mysql.getConnection();
+      await conn.beginTransaction();
+      const [ result ] = await conn.query(sql);
+      rt.ok = true;
+      rt.msg = 'ok';
+      rt.result = result;
+      await conn.commit();
+      conn.release();
+    }
+  } catch (err) {
+    console.error('userTable/user_updae Error!!');
+    console.error(err);
+    rt.msg = 'user_update Error';
+    rt.result = err.message;
+    if (conn !== null) {
+      await conn.rollback();
+      conn.release();
+    }
   }
   res.send(rt);
 })
