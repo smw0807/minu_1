58d56229fd8d94bba239f92a079ec67d598e3148
smw0807 <smw0807@gmail.com>
2022-04-17 19:34:14 +0900

    웹소켓 서버 테스트

---
+++ node/websocket/chat/app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const webSocket = require('./socket');
+const indexRouter = require('./routes');
+
+const app = express();
+app.set('port', process.env.port || 8081);
+app.set('vie engine', 'html');
+nunjucks.configure('views', {
+  express: app,
+  watch: true
+});
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser(process.env.cookie_secret));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.cookie_secret,
+  cookie: {
+    httpOnly: true,
+    secure: false
+  }
+}));
+
+app.use('/', indexRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.messate = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+})
+
+const server = app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
+})
+
+webSocket(server);
