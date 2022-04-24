c35e47e83bd285b27ecf029b21341a90529d5d56
smw0807 <smw0807@gmail.com>
2022-04-24 16:08:38 +0900

    책 읽던 중 예제

---
+++ book/refactoring/7/2.js
@@ -0,0 +1,33 @@
+const { cloneDeep } = require('lodash');
+const dataSet = require('./2dataSet');
+console.dir(dataSet, {depth: 5});
+// console.table(dataSet);
+console.log('------------------');
+// console.log(dataSet['2022'].usages['2019']['3']);
+
+class CustomerData {
+  constructor(data) {
+    this._data = data;
+  }
+  //읽기
+  getRawData() {
+    return cloneDeep(this._data);
+  }
+  usage(customerID, year, month) {
+    return this._data[customerID].usages[year][month];
+  }
+  //쓰기
+  setUsage(customerID, year, month, amount) {
+    this._data[customerID].usages[year][month] = amount;
+  }
+
+}
+
+const customer = new CustomerData(dataSet);
+// console.log(customer);
+const getData = customer.usage('2022', '2019', '5');
+console.log(getData);
+customer.setUsage('2022', '2019', '5', 10);
+const test = customer.usage('2022', '2019', '5');
+console.log(getData); //18
+console.log(test); //10
