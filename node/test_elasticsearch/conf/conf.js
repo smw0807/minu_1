19b202f03321c6c8ee103607da118f3bda30e09e
smw0807 <smw0807@gmail.com>
2022-05-09 19:43:06 +0900

    불필요 파일 제거

--- node/test_elasticsearch/conf/conf.js
+++
@@ -1,8 +0,0 @@
-const conf = {
-    els_id: 'test',
-    els_pw: 'test',
-    els_ip: 'localhost',
-    els_port: '9200'
-};
-
-module.exports = conf;
--- node/test_elasticsearch/query/userQuery.js
+++
@@ -1,7 +0,0 @@
-exports.search = {
-    query: {
-        bool: {
-            must: []
-        }
-    }
-}
