8bd45b533ddb62726f255ca6a011614c48c709c3
smw0807 <smw0807@gmail.com>
2022-05-13 21:56:33 +0900

    json 배열에서 원하는 데이터 찾기 재귀함수를 통해 뽑기... 완성은 했지만 뭔가 좀 아쉬움

--- node/test/find-data-in-json.js
+++ node/test/find-data-in-json.js
@@ -5,40 +5,32 @@ const fs = require('fs');
 const json = fs.readFileSync('./data.json', 'utf-8');
 
  function findDocument(aCode) {
-  console.log(aCode);
   const jsonArr = JSON.parse(json);
   let result = null;
-  result = jsonArr.find( (doc, idx) => {
+  jsonArr.map( (doc) => {
     if (doc.code === aCode) {
-      return doc;
+      result =  doc;
     } else {
-      const data = searchChildren(doc.children);
-      console.log('e',data);
-      if (data) {
-        return data;
-      }
+      searchChildren(doc.children);
     }
   })
 
   function searchChildren(arr) {
-    let rt = null;
-    if (Array.isArray(arr)) {
-      rt = arr.find( (doc) => {
+    if (arr.length !== 0) {
+      arr.map( (doc) => {
         if (doc.code === aCode) {
-          console.log('c', doc);
-          return doc;
+          result = doc;
         } else {
           return searchChildren(doc.children);
         }
       })
-      console.log('d', rt);
-      return rt;
     }
   }
 
 
   return result;
 }
-
+console.time('test');
 const findData =  findDocument(3)
+console.timeEnd('test');
 console.log('findData : ', findData);
