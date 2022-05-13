ae677ba67d64d7f409281032e17ddf76ac012014
smw0807 <smw0807@gmail.com>
2022-05-13 09:21:23 +0900

    책 읽던 준 예제... 클래스는 아직도 이해를 못하겠네

--- book/refactoring/7/5.js
+++ book/refactoring/7/5.js
@@ -5,11 +5,25 @@ class Person {
   constructor(name, telephoneNumber, officeAreaCode, officeNumber) {
     this._name = name;
     this._telephoneNumber = telephoneNumber;
-    this._officeAreaCode = officeAreaCode;
-    this._officeNumber = officeNumber;
+    // this._officeAreaCode = officeAreaCode;
+    // this._officeNumber = officeNumber;
+    this._telephoneNumber = new TelephoneNumber();
   }
 
   get name() { return this._name; }
   set name(arg) { this._name = arg; }
+  get officeAreaCode() { return this._telephoneNumber.officeAreaCode; }
+  set officeAreaCode(arg) { this._telephoneNumber.officeAreaCode = arg; }
   
 }
+class TelephoneNumber {
+  // constructor()
+  get officeAreaCode() { return this._officeAreaCode; }
+  set officeAreaCode(arg) { this._officeAreaCode = arg; }
+}
+
+const a = new Person('minwoo', '01011112222', 2, 5);
+console.log(a);
+// a.of
+a.officeAreaCode = 3;
+console.log(a);
