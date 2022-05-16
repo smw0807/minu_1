715bc0534dc650f8ea1299ccc8a04d663566ad01
smw0807 <smw0807@gmail.com>
2022-05-16 19:56:52 +0900

    벌크 만들기..

--- node/es_functions/run_make_bulk.js
+++ node/es_functions/run_make_bulk.js
@@ -5,16 +5,16 @@ const fs = require('fs');
 const es_client = require('./client');
 
 const is_id = true
-const index_name = 'ni_manage';
+const index_name = 'ni_setting_test';
 
 const query = {
-  size: 100,
-  query : {
-    bool : {
-      must : [
+  "size": 100,
+  "query": {
+    "bool": {
+      "must": [
         {
-          term : {
-            type : "cmn_code"
+          "term": {
+            "type": "org"
           }
         }
       ]
@@ -23,24 +23,28 @@ const query = {
 }
 
 async function run() {
-  let rs = await es_client.search({
-    index: index_name,
-    type: '_doc',
-    body: query,
-    scroll: '1m'
-  })
-  let bulk = '';
-  while(rs.hits.hits.length > 0) {
-    rs.hits.hits.map( (doc) => {
-      if (is_id) {
-        bulk += JSON.stringify({index: {_index: doc._index, _type: doc._type, _id: doc._id}}) + '\n';
-      } else {
-        bulk += JSON.stringify({index: {_index: doc._index, _type: doc._type}}) + '\n';
-      }
-      bulk += JSON.stringify(doc._source) + '\n';
+  try {
+    let rs = await es_client.search({
+      index: index_name,
+      body: query,
+      scroll: '1m'
     })
-    rs = await es_client.scroll({ scrollId: rs._scroll_id, scroll: '1m'});
+    let bulk = '';
+    while(rs.hits.hits.length > 0) {
+      rs.hits.hits.map( (doc) => {
+        if (is_id) {
+          bulk += JSON.stringify({index: {_index: doc._index, _id: doc._id}}) + '\n';
+        } else {
+          bulk += JSON.stringify({index: {_index: doc._index }}) + '\n';
+        }
+        bulk += JSON.stringify(doc._source) + '\n';
+      })
+      rs = await es_client.scroll({ scrollId: rs._scroll_id, scroll: '1m'});
+    }
+    fs.writeFileSync('./_bulk/' + index_name, bulk);
+  } catch (err) {
+    console.error(err.message);
+    console.error(err);
   }
-  fs.writeFileSync('./_bulk/' + index_name, bulk);
 }
 run();
