eba08281a0fd3ed833afe927321c098d1b37d47c
smw0807 <smw0807@gmail.com>
2022-05-17 09:15:35 +0900

    책 읽던 중 예제... 내용은 노션에 정리

---
+++ book/refactoring/7/9.js
@@ -0,0 +1,18 @@
+function foundPerson(people) {
+  for (let i = 0; i < people.length; i++) {
+    if (people[i] === 'Don') {
+      return 'Don';
+    }
+    if (people[i] === 'John') {
+      return 'John';
+    }
+    if (people[i] === 'Kent') {
+      return 'Kent';
+    }
+  }
+}
+
+function foundPerson(people) {
+  const candidates = ['Don', 'John', 'Kent'];
+  return people.find( p => candidates.includes(p) || '');
+}
