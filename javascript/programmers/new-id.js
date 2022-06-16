d5b06fd5ab7f6fbd1e9e719aa9cbb73b5a37eb04
smw0807 <smw0807@gmail.com>
2022-06-16 17:17:52 +0900

    프로그래머스 레벨1 신규 아이디 추천

--- javascript/programmers/new-id.js
+++ javascript/programmers/new-id.js
@@ -1,54 +1,60 @@
 /**
  * 프로그래머스 레벨1 신규 아이디 추천
  * 내가 짠 소스...
+    1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
+    2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+    3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+    4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+    5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+    6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+        만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
+    7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
  */
 
 function solution(new_id) {
   let answer = new_id.toLowerCase()
-              .replace(/[^\\a-z\\0-9\\-\\_\\.]/g, '')
-              .replace(/(\.){2,}/g, '.');
-  if (answer === '') {
-    answer = 'a';
-  }
-  answer = answer.replace(/^./g, '')
-          .replace(/.$/g, '');
-
-  if (answer.length > 15) {
-    answer = answer.substring(0, 15);
-  }
-
+              .replace(/[^a-z0-9-_.]/g, '') //1
+              .replace(/(\.){2,}/g, '.') //2
+              .replace(/^[.]/g, '') //3
+              .replace(/[.]$/g, ''); //4
   
-
-  console.log(answer);
-  // const changeLower = new_id.toLowerCase();
-  // console.log(changeLower);
-  // const dotdot = changeLower.replace(/(\.){2,}/g, '.');
-  
-  // console.log(dotdot);
-  // const delStartDot = dotdot.replace(/^./g, '');
-  // console.log(delStartDot);
-  // const delEndDot = dotdot.replace(/.$/g, '');
-  // console.log(delEndDot);
-  // const emptyCheck = delEndDot === '' ? 'a' : delEndDot;
-  // console.log(emptyCheck);
-  // console.log(emptyCheck.length);
-  // let answer = '';
-  // const changeLower = new_id.toLowerCase();
-  // console.log(changeLower);
-  // const dotdot = changeLower.replace(/(\.){2,}/g, '.');
-
-  // console.log(dotdot);
-  // const delStartDot = dotdot.replace(/^./g, '');
-  // console.log(delStartDot);
-  // const delEndDot = dotdot.replace(/.$/g, '');
-  // console.log(delEndDot);
-  // const emptyCheck = delEndDot === '' ? 'a' : delEndDot;
-  // console.log(emptyCheck);
-  // console.log(emptyCheck.length);
-
+  if (answer === '') answer = 'a'; //5
+  answer = limit_id_length(answer); //6
+  answer = check_id_length(answer); //7
   return answer;
 }
 
-const data = '...!@BaT#*..y.abcdefghijklm.';
-// const data = '';
-solution(data);
+function limit_id_length (v) {
+  let result = v;
+  if (result.length > 15) {
+    result = result.substring(0, 15).replace(/[.]$/g, '');
+  }
+  return result;
+}
+
+function check_id_length (v) {
+  let result = v;
+  if (result.length <= 2) {
+    result = result + result.substring(result.length -1 , result.length);
+    return check_id_length(result);
+  }
+  return result;
+}
+
+const data = "...!@BaT#*..y.abcdefghijklm."; //"bat.y.abcdefghi"
+const expectedValue = "bat.y.abcdefghi";
+
+// const data = "z-+.^."; //"z--"
+// const expectedValue = "z--";
+
+// const data = "=.="; //"aaa"
+// const expectedValue = "aaa";
+
+// const data = "123_.def"; //"123_.def"
+// const expectedValue = "123_.def";
+
+// const data = "abcdefghijklmn.p"; //"abcdefghijklmn"
+// const expectedValue = "abcdefghijklmn";
+
+const result = solution(data);
+console.log(result === expectedValue);
