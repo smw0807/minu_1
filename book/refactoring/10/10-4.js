b9ec31fb7363362d376fac80ff5b49d2991a9e47
smw0807 <smw0807@gmail.com>
2022-07-01 18:15:14 +0900

    책 읽던 중 예제

--- book/refactoring/10/10-4.js
+++ book/refactoring/10/10-4.js
@@ -8,24 +8,24 @@ class Bird {
   }
 
   get airSpeedVelocity() {
-    switch(this.type) {
-      case 35:
-        return '보통이다';
-      case 40 - 2 * this.numberOfCounts:
-        return (bird.numberOfCounts > 2) ? '지쳤다' : '보통이다';
-      case '노르웨이 파랑 앵무':
-        return (this.isNailed) ? 0 : 10 + this.voltage / 10;
-      default:
-        return null;
-    }
+    return null;
   }
 }
 
 
 //서브클래스의 인스턴스를 만들어줄 팩터리 함수?
-function plumage(bird) {
-  return createBird(bird).plumage;
+function plumage(birds) {
+  // return createBird(birds).plumage;
+  return new Map(birds
+    .map(b => createBird(b))
+    .map(bird => [bird.name, bird.plumage]));
 }
+function speeds(birds) {
+  return new Map(birds
+    .map(b => createBird(b))
+    .map(bird => [bird.name, bird.airSpeedVelocity]));
+}
+
 //서브클래스의 인스턴스를 만들어줄 팩터리 함수?
 function airSpeedVelocity(bird) {
   return createBird(bird).airSpeedVelocity;
@@ -43,11 +43,13 @@ function createBird(bird) {
       return new Bird(bird);
   }
 }
-
 //종별 서브클래스
 class EuropeanSwallow extends Bird {
   get plumage() {
     return '보통이다';
+  }
+  get airSpeedVelocity() {
+    return 35;
   }
 }
 //종별 서브클래스
@@ -55,10 +57,27 @@ class AfricanSwallow extends Bird {
   get plumage() {
     return (this.numberOfCounts > 2) ? '지쳤다' : '보통이다';
   }
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCounts;
+  }
 }
 //종별 서브클래스
 class NorwegianBlueParrot extends Bird {
   get plumage() {
     return (this.voltage > 100) ? '그을렸다' : '예쁘다';
   }
+  get airSpeedVelocity() {
+    return (this.isNailed) ? 0 : 10 + this.voltage / 10;
+  }
 }
+
+const a1 = {
+  type: '유럽 제비',
+  numberOfCounts: 1,
+  voltage: 50,
+  isNailed: true
+}
+const bird_1 = new NorwegianBlueParrot(a1);
+console.log(bird_1);
+console.log(bird_1.plumage);
+console.log(bird_1.airSpeedVelocity);
