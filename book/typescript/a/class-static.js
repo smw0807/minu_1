8899767f4cf2ac7176c19b1e084f8042e65cf17a
Minu Song <smw0807@gmail.com>
2022-08-10 23:46:57 +0900

    타입스크립트 책 부록 예제 + 연습 예제 따라 친 것도 있고, 내 생각을 더해서 한 것도 있고...

---
+++ book/typescript/a/class-static.js
@@ -0,0 +1,26 @@
+class Person {
+  static counter = 0; //정적 프로퍼티
+  constructor(name, cnt) {
+    this._name = name;
+    this._cnt = cnt;
+  }
+  get name() { return this._name; }
+  set name(arg) { this._name = arg; }
+
+  get cnt(){ return this._cnt; }
+  set cnt(arg) { this._cnt = arg; }
+
+  printCounter() {
+    console.log(Person.counter); //클래스 이름으로 정적 프로퍼티를 참조한다.
+  }
+}
+
+const a = new Person('a', 0);
+const b = new Person('b', 0);
+
+Person.counter++; //클래스 이름으로 정적 프로퍼티에 접근?
+a.printCounter();
+Person.counter++;
+b.printCounter(); //클래스 이름으로 정적 프로퍼티에 접근?
+console.log(a.counter); //안나옴
+console.log(b.counter); //안나옴
