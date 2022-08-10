641582d5a35ec5037e4565a6fbaab726a2781112
Minu Song <smw0807@gmail.com>
2022-08-11 00:02:31 +0900

    static 메서드

--- book/typescript/a/class-static.js
+++ book/typescript/a/class-static.js
@@ -1,5 +1,9 @@
 class Person {
   static counter = 0; //정적 프로퍼티
+  static printConsole(msg) {
+    if (msg) console.log(`this is static method!! msg : ${msg}`);
+    else console.log('this is static method!!');
+  }
   constructor(name, cnt) {
     this._name = name;
     this._cnt = cnt;
@@ -13,6 +17,9 @@ class Person {
   printCounter() {
     console.log(Person.counter); //클래스 이름으로 정적 프로퍼티를 참조한다.
   }
+  printStaticMethod() {
+    Person.printConsole();
+  }
 }
 
 const a = new Person('a', 0);
@@ -20,7 +27,10 @@ const b = new Person('b', 0);
 
 Person.counter++; //클래스 이름을 참조로 정적 프로퍼티에 접근?
 a.printCounter();
-Person.counter++;
-b.printCounter(); //클래스 이름을 참조로 정적 프로퍼티에 접근?
+Person.counter++; //클래스 이름을 참조로 정적 프로퍼티에 접근?
+b.printCounter();
 console.log(a.counter); //안나옴, 인스턴스에 없기 때문에 접근 안됨
 console.log(b.counter); //안나옴, 인스턴스에 없기 때문에 접근 안됨
+
+Person.printConsole('kkkk');
+a.printStaticMethod();
