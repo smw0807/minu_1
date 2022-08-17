2084efeffec3a0510faa906993473402acafa85e
smw0807 <smw0807@gmail.com>
2022-08-17 13:52:48 +0900

    map test

---
+++ javascript/map/dataChange.js
@@ -0,0 +1,36 @@
+const myTree = [
+  {
+    index: 0,
+    branch: 'a',
+    select: false,
+    isUse: 'ok',
+    title: 'hi111',
+    content: 'hihi',
+  },
+  {
+    index: 1,
+    branch: 'a',
+    select: false,
+    isUse: 'ok',
+    title: 'hi222',
+    content: 'hihi',
+  },
+  {
+    index: 2,
+    branch: 'a',
+    select: false,
+    isUse: 'ok',
+    title: 'hi333',
+    content: 'hihi',
+  },
+]
+
+function change(idx) {
+  return myTree.map((v, k) => {
+    if (k === idx) v.select = true;
+    else v.select = false;
+  })
+}
+change(1);
+
+
