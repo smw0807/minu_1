d7e83a3e22a8e214bdbe5ba1f16dde62cf89f5dc
Minu Song <smw0807@gmail.com>
2022-08-21 21:28:29 +0900

    노드 디자인 패턴 바이블 읽던 중 예제 내용 정리는 노션에

---
+++ book/node-pattern-bible/chapter02/modify-module/main.mjs
@@ -0,0 +1,14 @@
+import fs from 'fs'; // 1
+import { mockEnable, mockDisable } from './mock-read-file.mjs';
+
+mockEnable(Buffer.from('Hello World')); // 2
+
+fs.readFile('fake-path', (err, data) => { // 3
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(data.toString()); // 'Hello world'
+})
+
+mockDisable();
