d61afb1a54293444252abe3021628d82bc04fb2e
Minu Song <smw0807@gmail.com>
2022-09-25 19:16:04 +0900

    노드 디자인 패턴 바이블 - crypto를 이용한 스트림

--- book/node-pattern-bible/chapter06/receive-send/gzip-receive.mjs
+++ book/node-pattern-bible/chapter06/receive-send/gzip-receive.mjs
@@ -2,12 +2,19 @@ import { createServer } from 'http';
 import { createWriteStream } from 'fs';
 import { createGunzip } from 'zlib';
 import { basename, join } from 'path';
+import { createDecipheriv, randomBytes } from 'crypto';
+
+const secret = randomBytes(24);
+console.log(`Generated secret: ${secret.toString('hex')}`);
 
 const server = createServer((req, res) => {
   const filename = basename(req.headers['x-filename']);
+  const iv = Buffer.from(req.headers['x-initialization-vector'], 'hex');
+  console.log('iv : ', iv);
   const destFilename = join('receive_files', filename);
   console.log(`File request received: ${filename}`);
   req
+    .pipe(createDecipheriv('aes192', secret, iv))
     .pipe(createGunzip())
     .pipe(createWriteStream(destFilename))
     .on('finish', () => {
--- book/node-pattern-bible/chapter06/receive-send/gzip-send.mjs
+++ book/node-pattern-bible/chapter06/receive-send/gzip-send.mjs
@@ -2,8 +2,14 @@ import { request } from 'http';
 import { createGzip } from 'zlib';
 import { createReadStream } from 'fs';
 import { basename } from 'path';
+import { createCipheriv, randomBytes } from 'crypto';
 
 const fileName = process.argv[2];
+console.log(process.argv[3]);
+const secret = Buffer.from(process.argv[3], 'hex');
+console.log('secret : ', secret);
+const iv = randomBytes(16);
+console.log('iv : ', iv);
 
 const httpRequestOptions = {
   hostname: 'localhost',
@@ -14,6 +20,8 @@ const httpRequestOptions = {
     'Content-Type': 'application/octet-stream',
     'Content-Encoding': 'gzip',
     'X-Filename': basename(fileName),
+    //초기화 벡터를 HTTP헤더로 서버에 전달한다.
+    'X-Initialization-Vector': iv.toString('hex'),
   },
 };
 
@@ -23,6 +31,8 @@ const req = request(httpRequestOptions, res => {
 
 createReadStream(fileName)
   .pipe(createGzip())
+  //Gzip 단계 이후 데이터를 암호화 한다.
+  .pipe(createCipheriv('aes192', secret, iv))
   .pipe(req)
   .on('finish', () => {
     console.log('File successfulLy sent!!');
