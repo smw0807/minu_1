10511b7c39966f5a5bebcbf06ac193ecbce94e06
Minu Song <smw0807@gmail.com>
2022-10-03 15:52:58 +0900

    PassThrouhg 적용 테스트

--- book/node-pattern-bible/chapter06/receive-send/gzip-send.mjs
+++ book/node-pattern-bible/chapter06/receive-send/gzip-send.mjs
@@ -1,4 +1,5 @@
 import { request } from 'http';
+import { PassThrough } from 'stream';
 import { createGzip } from 'zlib';
 import { createReadStream } from 'fs';
 import { basename } from 'path';
@@ -29,8 +30,19 @@ const req = request(httpRequestOptions, res => {
   console.log(`Server response : ${res.statusCode}`);
 });
 
+let bytesWritten = 0;
+const monitor = new PassThrough();
+monitor.on('data', chunk => {
+  bytesWritten += chunk.length;
+  console.log(`${bytesWritten}`);
+});
+monitor.on('finish', () => {
+  console.log(`${bytesWritten} bytes writen.`);
+});
+
 createReadStream(fileName)
   .pipe(createGzip())
+  .pipe(monitor)
   //Gzip 단계 이후 데이터를 암호화 한다.
   .pipe(createCipheriv('aes192', secret, iv))
   .pipe(req)
