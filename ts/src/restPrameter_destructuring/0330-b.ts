b7f1e4169979de95b18e94eae7850b31b019941a
Minu Song <smw0807@gmail.com>
2023-04-02 22:05:33 +0900

    타입스크립트 디스트처링 테스트

--- ts/src/restPrameter_destructuring/0330-b.ts
+++ ts/src/restPrameter_destructuring/0330-b.ts
@@ -8,3 +8,28 @@ function maxNumber(...a: number[]): number {
 
 const number = maxNumber(6, 3, 7, 2);
 console.log(number);
+
+interface IB {
+  b: boolean;
+  n: number;
+  s: string;
+  a: number[];
+}
+type bType = (boolean | number | string | number[])[];
+function b(...a: bType) {
+  console.log(a);
+  //[ true, 1, 'a', [ 1, 2, 3 ] ]
+}
+function b2(...a: IB[]) {
+  console.log(a);
+  //[ { b: false, n: 2, s: 'b', a: [ 4, 5, 6 ] } ]
+}
+
+b(true, 1, 'a', [1, 2, 3]);
+// b2(true, 2, 'b', [4, 5, 6]);
+b2({
+  b: false,
+  n: 2,
+  s: 'b',
+  a: [4, 5, 6],
+});
