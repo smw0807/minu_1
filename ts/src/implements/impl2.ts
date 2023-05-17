48fd271e8c6b416642904ae67954a55f6a043696
Minu Song <smw0807@gmail.com>
2023-05-17 20:59:01 +0900

    주석

--- ts/src/implements/impl2.ts
+++ ts/src/implements/impl2.ts
@@ -4,10 +4,10 @@ interface ICar {
   tax: (price: number) => number;
 }
 
-class Car implements ICar {
-  model; //타입을 지정해주지 않으면 any 타입이됨
-  tax(a) {
-    //타입을 지정해주지 않으면 any 타입이됨
-    return a * 0.1;
-  }
-}
+// class Car implements ICar {
+//   model; //타입을 지정해주지 않으면 any 타입이됨
+//   tax(a) {
+//     //타입을 지정해주지 않으면 any 타입이됨
+//     return a * 0.1;
+//   }
+// }
