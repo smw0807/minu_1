3a13c109ea53be431519ffb480d1c69efa6fbc2e
Minu Song <smw0807@gmail.com>
2023-09-03 18:34:46 +0900

    디자인패턴바이블 > 반복가능자 프로토콜

--- book/node-pattern-bible/chapter09/4-iterator/iterable/index.mjs
+++ book/node-pattern-bible/chapter09/4-iterator/iterable/index.mjs
@@ -4,7 +4,8 @@ const matrix2x2 = new Matrix([
   ['11', '12'],
   ['21', '22'],
 ]);
-
+matrix2x2.set(1, 0, '31');
+matrix2x2.set(1, 1, '32');
 const iterator = matrix2x2[Symbol.iterator]();
 let iteratorResult = iterator.next();
 while (!iteratorResult.done) {
