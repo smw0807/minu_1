b44f8d9676e7c053491100460ab100c788c48094
Minu Song <smw0807@gmail.com>
2024-01-22 23:49:02 +0900

    오타 수정

--- book/node-pattern-bible/chapter13/2/pubsub-chat-redis/index.js
+++ book/node-pattern-bible/chapter13/2/pubsub-chat-redis/index.js
@@ -1,29 +1,14 @@
 import { createServer } from 'http';
-import staticHadler from 'serve-handler';
+import staticHandler from 'serve-handler';
 import ws from 'ws';
 import Redis from 'ioredis'; // 1
-
-// const redisOptions = {
-//   port: 6379,
-//   host: '127.0.0.1',
-//   family: 4,
-//   db: 0,
-// };
 
 const redisSub = new Redis();
 const redisPub = new Redis();
 
-redisSub.get('chat_messages', (err, result) => {
-  if (err) {
-    console.error('Error getting key:', err);
-  } else {
-    console.log('Value:', result);
-  }
-});
-
 // 정적인 파일들을 서비스함
 const server = createServer((req, res) => {
-  return staticHadler(req, res, { public: 'www' });
+  return staticHandler(req, res, { public: 'www' });
 });
 
 const wss = new ws.Server({ server });
@@ -32,7 +17,7 @@ wss.on('connection', client => {
   client.on('message', msg => {
     console.log(`Message: ${msg}`);
     // 2
-    redisPub.publish('chat_message', msg);
+    redisPub.publish('chat_messages', msg);
   });
 });
 
