2adc97988e0e10b2fc0a2584033c3a347630aa13
Minu Song <smw0807@gmail.com>
2024-01-29 23:44:53 +0900

    노드디자인패턴 > AMQP

---
+++ book/node-pattern-bible/chapter13/2/pubsub-amqp/index.js
@@ -0,0 +1,60 @@
+import { createServer } from 'http';
+import staticHandler from 'serve-handler';
+import ws from 'ws';
+import amqp from 'amqplib';
+import JSONStream from 'JSONStream';
+import superagent from 'superagent';
+
+const httpPort = process.argv[2] || 8080;
+
+async function main() {
+  const connection = await amqp.connect('amqp://localhost');
+  const channel = await connection.createChannel();
+  await channel.assertExchange('chat', 'fanout');
+  const { queue } = await channel.assertQueue(`chat_srv_${httpPort}`, { exclusive: true }); //1
+  await channel.bindQueue(queue, 'chat');
+
+  //2
+  channel.consume(
+    queue,
+    msg => {
+      msg = msg.content.toString();
+      console.log(`From queue: ${msg}`);
+      broadcast(msg);
+    },
+    { noAck: true }
+  );
+
+  const server = createServer((req, res) => {
+    return staticHandler(req, res, { public: 'www' });
+  });
+
+  const wss = new ws.Server({ server });
+  wss.on('connection', client => {
+    console.log('Client connected');
+
+    client.on('message', msg => {
+      console.log('Message: ${msg}');
+      channel.publish('chat', '', Buffer.from(msg)); //3
+    });
+
+    // 이력 조회 서비스
+    superagent //4
+      .get('http://localhost:8090')
+      .on('error', err => console.error(err))
+      .pipe(JSONStream.parse('*'))
+      .on('data', msg => client.send(msg));
+  });
+
+  function broadcast(msg) {
+    for (const client of wss.clients) {
+      if (client.readyState === ws.OPEN) {
+        client.send(msg);
+      }
+    }
+  }
+
+  server.listen(httpPort);
+}
+
+main().catch(err => console.error(err));
