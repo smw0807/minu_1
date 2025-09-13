ab9fa06622b38d4547cd067d49749783315a483f
smw0807 <smw0807@gmail.com>
2025-09-13 21:25:27 +0900

    강의

---
+++ codefactory/1_basics/3_declaring_variable.js
@@ -0,0 +1,5 @@
+/**
+ * Variable 선언하기
+ */
+var name = 'John';
+console.log(name);
---
+++ codefactory/1_basics/4_naming_conventions.js
@@ -0,0 +1,38 @@
+/**
+ * Naming Conventions
+ *
+ * 1) 일반적으로 영어(라틴문자)를 사용하며 문자와 숫자를 모두 사용 할 수 있다.
+ * 2) 특수기호는 언더스코어와 달러를 사용 할 수 있다.
+ * 3) 숫자로 이름을 시작할 수 없다.
+ * 4) 키워드는 변수명으로 사용 할 수 없다.(예약어)
+ */
+let codefactory = '코드팩토리';
+var $ive = '아이브';
+const _yuJin = '안유진';
+
+console.log(codefactory);
+console.log($ive);
+console.log(_yuJin);
+
+/**
+ * Naming Conventions 2
+ * 1) camelCase - 대부분의 언어에서 많이 사용
+ * 2) snake_case - 주로 Python에서 많이 사용
+ * 3) PascalCase - 주로 Java에서 많이 사용
+ * 4) kebab-case - 주로 CSS에서 많이 사용
+ * 5) UPPER_CASE - 주로 상수에서 많이 사용
+ * 6) LOWER_CASE - 주로 변수에서 많이 사용
+ * 7) SCREAMING_SNAKE_CASE - 주로 상수에서 많이 사용
+ * 8) kebab-case - 주로 CSS에서 많이 사용
+ * 9) UPPER_CASE - 주로 상수에서 많이 사용
+ * 10) LOWER_CASE - 주로 변수에서 많이 사용
+ *
+ *  - 예시
+ *    - camelCase: userName, userEmail, userPassword
+ *    - snake_case: user_name, user_email, user_password
+ *    - PascalCase: UserName, UserEmail, UserPassword
+ *    - kebab-case: user-name, user-email, user-password
+ *    - UPPER_CASE: USER_NAME, USER_EMAIL, USER_PASSWORD
+ *    - LOWER_CASE: user_name, user_email, user_password
+ *    - SCREAMING_SNAKE_CASE: USER_NAME, USER_EMAIL, USER_PASSWORD
+ */
