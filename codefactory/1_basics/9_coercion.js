26d59e71eaaf9312208eaff2d08e78ecb284f6be
smw0807 <smw0807@gmail.com>
2025-09-14 21:09:34 +0900

    coercion

---
+++ codefactory/1_basics/9_coercion.js
@@ -0,0 +1,17 @@
+console.log(!!'hi');
+console.log(!!'');
+console.log(!!1);
+console.log(!!0);
+
+console.log('--------------------------------');
+const v = 'hi';
+console.log(v);
+console.log(!!v);
+
+/**
+ * 1) 아무 글자도 없는 String
+ * 2) 값이 없는 경우
+ * 3) 0
+ *
+ * 모두 false로 취급된다.
+ */
