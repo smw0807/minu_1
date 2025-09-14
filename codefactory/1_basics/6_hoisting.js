8769d779258adfcc2211f27e0d4a4d4927a878cc
smw0807 <smw0807@gmail.com>
2025-09-14 16:28:11 +0900

    hoisting

---
+++ codefactory/1_basics/6_hoisting.js
@@ -0,0 +1,20 @@
+console.log('Hello');
+console.log('World');
+
+// console.log(name); //undefined
+// var name = 'John';
+// console.log(name);
+
+// var name;
+// console.log(name); //undefined
+// name = 'John';
+// console.log(name); //John
+
+// let name;
+// console.log(name); //undefined
+// name = 'John';
+// console.log(name); //John
+
+console.log(name); //ReferenceError: name is not defined
+const name = 'John';
+console.log(name); //John
--- codefactory/README.md
+++ codefactory/README.md
@@ -43,3 +43,7 @@ console.log(symbol1 === symbol2);
 
 Map  
 key:value의 쌍으로 이루어져 있다.
+
+# Hoisting
+
+모든 변수 선언문이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상을 이야기한다.
